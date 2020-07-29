639a9b97f33c87b673235cb394197e64984828ec
Tsz Ning Cheung <cheung33156@yahoo.com>
2020-07-29 11:10:01 -0400

    Add project files.

---
+++ Main/Program.cs
@@ -0,0 +1,25 @@
+﻿// Copyright 2020 Tsz Ning Cheung 
+
+using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //URL for getting token https://finnhub.io/dashboard; 
+        const string token = "Tsk_750bc3b4e374437faf4e067b9799e806";
+        //const string token = "pk_4047e8513786474ba659893830c036ad";
+
+        IEX.Search.FluentRequest obj = new IEX.Search.FluentRequest();
+        string response = obj.Token(token)
+            .Command("search")
+            .Fragment("Apple")
+            .Execute()
+            .ToString();
+
+        Console.WriteLine(response);
+        Console.ReadLine();
+    }
+}
+

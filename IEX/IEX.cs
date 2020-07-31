acaa599fb8ce633526b38217c7c8431534e24396
Tsz Ning Cheung <cheung33156@yahoo.com>
2020-07-31 16:30:05 -0400

    Implemented code for getting company profile.

--- IEX/IEX.cs
+++ IEX/IEX.cs
@@ -3,86 +3,45 @@ using Newtonsoft.Json;
 using RestSharp;
 using System;
 using System.Collections.Generic;
-
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using System.Transactions;
 
 namespace IEX
 {
-    namespace Search
+    namespace Stock
     {
-        public class Result
+        public class Request : IRequest
         {
-            [JsonProperty("symbol")]
-            public string Symbol { get; set; }
-            [JsonProperty("securityName")]
-            public string SecurityName { get; set; }
-            [JsonProperty("securityType")]
-            public string SecurityType { get; set; }
-            [JsonProperty("region")]
-            public string Region { get; set; }
-            [JsonProperty("exchange")]
-            public string Exchange { get; set; }
-        }
-        public class Results : List<Result>
-        {
-            public new string ToString()
+            private readonly RequestBase request = new RequestBase();
+            public Request()
             {
-                return JsonConvert.SerializeObject(this, Formatting.Indented);
+                request.Category = "stock";
             }
-        }
-        class Request
-        {
-            public string Token { get; set; }
-            public string Command { get; set; }
-            public string Fragment { get; set; }
-        }
-        public class FluentRequest
-        {
-            private Request request = new Request();
-            public FluentRequest Token(string token)
+            public Request Symbol(string symbol)
             {
-                request.Token = token;
+                request.Symbol = symbol;
                 return this;
             }
-            public FluentRequest Command(string command)
+            public Request Command(string command)
             {
                 request.Command = command;
                 return this;
             }
-            public FluentRequest Fragment(string fragment)
-            {
-                request.Fragment = fragment;
-                return this;
-            }
-            public new string ToString()
-            {
-                return request.ToString();
-            }
-            public Results Execute()
-            {
-                const string urlBase = "https://sandbox.iexapis.com/stable/";
 
-                string Url = String.Format("{0}/{1}/{2}?token={3}",
-                                urlBase,
+            public override string FormatURL()
+            {
+                return String.Format("{0}/{1}/{2}/{3}?token={4}",
+                                Configs.urlBase,
+                                request.Category,
+                                request.Symbol,
                                 request.Command,
-                                request.Fragment,
-                                request.Token);
-
-                var client = new RestClient(Url);
-                var restRequest = new RestRequest(Method.GET);
-                IRestResponse response = client.Execute(restRequest);
-
-                Results results = JsonConvert.DeserializeObject<Results>(response.Content);
-
-                return results;
+                                Configs.AccessToken);
             }
+
 
         }
-    }
-
-
-    namespace Company
-    {
-        public class Result
+        public class Results : IResults
         {
             [JsonProperty("symbol")]
             public string Symbol { get; set; }
@@ -99,7 +58,7 @@ namespace IEX
             [JsonProperty("description")]
             public string Description { get; set; }
             [JsonProperty("CEO")]
-            public int CEO { get; set; }
+            public string CEO { get; set; }
             [JsonProperty("securityName")]
             public string SecurityName { get; set; }
             [JsonProperty("issueType")]
@@ -124,5 +83,125 @@ namespace IEX
             public string Phone { get; set; }
         }
 
+        public class Rest
+        {
+            IRequest Request;
+            IResults Results;
+
+            public Rest(IRequest request, IResults results)
+            {
+                // dependency injection
+                this.Request = request;
+                this.Results = results;
+            }
+            public String DoRequest()
+            {
+                string Url = Request.FormatURL();
+
+                var client = new RestClient(Url);
+                var restRequest = new RestRequest(Method.GET);
+                IRestResponse response = client.Execute(restRequest);
+
+
+                Results = JsonConvert.DeserializeObject<Results> (response.Content);
+                return Results.ToString();
+            }
+        }
     }
+
+    namespace Search
+    {
+        class _Request
+        {
+            public string Command { get; set; }
+            public string Fragment { get; set; }
+        }
+        public class Request
+        {
+            private readonly _Request request = new _Request();
+            public Request Command(string command)
+            {
+                request.Command = command;
+                return this;
+            }
+            public Request Fragment(string fragment)
+            {
+                request.Fragment = fragment;
+                return this;
+            }
+            public new string ToString()
+            {
+                return request.ToString();
+            }
+            public Results Execute()
+            {
+
+                string Url = String.Format("{0}/{1}/{2}?token={3}",
+                                Configs.urlBase,
+                                request.Command,
+                                request.Fragment,
+                                Configs.AccessToken);
+
+                var client = new RestClient(Url);
+                var restRequest = new RestRequest(Method.GET);
+                IRestResponse response = client.Execute(restRequest);
+
+                Results results = JsonConvert.DeserializeObject<Results>(response.Content);
+
+                return results;
+            }
+
+        }
+        public class _Result
+        {
+            [JsonProperty("symbol")]
+            public string Symbol { get; set; }
+            [JsonProperty("securityName")]
+            public string SecurityName { get; set; }
+            [JsonProperty("securityType")]
+            public string SecurityType { get; set; }
+            [JsonProperty("region")]
+            public string Region { get; set; }
+            [JsonProperty("exchange")]
+            public string Exchange { get; set; }
+
+        }
+        public class Results : List<_Result>
+        {
+            public new string ToString()
+            {
+                return JsonConvert.SerializeObject(this, Formatting.Indented);
+            }
+        }
+
+    }
+
+    public abstract class IRequest
+    {
+        public abstract string FormatURL();
+    }
+    public abstract class IResults
+    {
+        public new string ToString()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+
+    public class RequestBase
+    {
+        public string Category { get; set; }
+        public string Command { get; set; }
+        public string Symbol { get; set; }
+    }
+
+    public class Configs
+    {
+        public static string AccessToken { get; set; }
+        public static string urlBase = "https://sandbox.iexapis.com/stable";
+    }
+
+
+
 }
+
--- Main/Program.cs
+++ Main/Program.cs
@@ -1,25 +1,30 @@
 ﻿// Copyright 2020 Tsz Ning Cheung 
 
 using System;
+using IEX;
+using IEX.Stock;
 
 
 class Program
 {
     static void Main(string[] args)
     {
-        //URL for getting token https://finnhub.io/dashboard; 
-        const string token = "Tsk_750bc3b4e374437faf4e067b9799e806";
-        //const string token = "pk_4047e8513786474ba659893830c036ad";
+        IEX.Configs.AccessToken = "Tsk_750bc3b4e374437faf4e067b9799e806";
 
-        IEX.Search.FluentRequest obj = new IEX.Search.FluentRequest();
-        string response = obj.Token(token)
-            .Command("search")
-            .Fragment("Apple")
-            .Execute()
-            .ToString();
+        //IEX.Search.Request obj = new IEX.Search.Request();
+        //string response = obj.Command("search")
+        //    .Fragment("Apple")
+        //    .Execute()
+        //    .ToString();
+        Request request = new Request();
+        request.Symbol("AAPL").Command("company");
 
-        Console.WriteLine(response);
+        Results results = new IEX.Stock.Results();
+
+        IEX.Stock.Rest action = new IEX.Stock.Rest(request, results);
+        Console.WriteLine(action.DoRequest());
         Console.ReadLine();
+
     }
 }
 
